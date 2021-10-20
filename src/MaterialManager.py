a96abb39c4ef2550c5e414cc0f58dada633d0fd4
osman-serhat-yilmaz <serhat.yilmaz@ug.bilkent.edu.tr>
2021-10-20 19:10:15 +0300

    add alloyX material

--- src/MaterialManager.py
+++ src/MaterialManager.py
@@ -19,10 +19,21 @@ class MaterialManager:
                 self.carManager.setMaterial(materialType.Carbon)
                 returnText = "Material is changed to Carbon."
 
+            case materialType.AlloyX:
+                self.carManager.setMaterial(materialType.AlloyX)
+                returnText = "Material is changed to AlloyX."
+
             case _:
-                returnText = "Material seletion is invalid."
+			returnText = "Material selection is invalid."
 
         return self._announceMaterial(returnText)
 
     def _announceMaterial(self, text: str) -> bool:
-        return self.announcer(text)
+		returnValue = False
+		try:
+			returnValue = self.announcer(text)
+		except:
+			print("Announcement of material type is failed.")
+			returnValue = False
+		finally:
+			return returnValue
